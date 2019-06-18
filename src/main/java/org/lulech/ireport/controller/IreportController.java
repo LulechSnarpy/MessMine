e95fb7ec93ce5a16c2ad369b463d679a368cb9fd
LulechSnarpy <1272045703@qq.com>
2019-06-18 15:44:39 +0800

    add an demo for another way export

--- src/main/java/org/lulech/ireport/controller/IreportController.java
+++ src/main/java/org/lulech/ireport/controller/IreportController.java
@@ -1,8 +1,14 @@
 package org.lulech.ireport.controller;
 
 import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -34,7 +41,30 @@ public class IreportController {
         Resource file = new UrlResource(Paths.get("upload-dir").resolve("report1.jasper").toUri());
         Map<String, Object> params = new HashMap<>();
         params.put("Title" , "Title1");
-        params.put("PageHeader", "PageHeader1");
+        params.put("PageHeader", "测试中文");
+        params.put("ColumnHeader", "ColumnsHeader1");
+        Collection<Map<String, Object>> data = new ArrayList<>();
+        Map<String, Object> k = new HashMap<>();
+        k.put("Title", "title0");
+        k.put("Value", "123");
+        data.add(k);
+        k = new HashMap<>();
+        k.put("Title", "title1");
+        k.put("Value", "456");
+        data.add(k);
+        JRDataSource dataSource = new JRBeanCollectionDataSource(data);
+        byte[] bytes = JasperRunManager.runReportToPdf(file.getInputStream(), params, dataSource);
+        Resource body = new ByteArrayResource(bytes);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename().replace("jasper", "pdf")+"\"").body(body);
+    }
+
+    @ResponseBody
+    @GetMapping("/export")
+    public ResponseEntity<Resource> export1() throws Exception {
+        Resource file = new UrlResource(Paths.get("upload-dir").resolve("report1.jasper").toUri());
+        Map<String, Object> params = new HashMap<>();
+        params.put("Title" , "Title1");
+        params.put("PageHeader", "测试中文");
         params.put("ColumnHeader", "ColumnsHeader1");
         Collection<Map<String, Object>> data = new ArrayList<>();
         Map<String, Object> k = new HashMap<>();
@@ -45,9 +75,16 @@ public class IreportController {
         k.put("Title", "title2");
         k.put("Value", "456");
         data.add(k);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         JRDataSource dataSource = new JRBeanCollectionDataSource(data);
-        byte[] bytes = JasperRunManager.runReportToPdf(file.getInputStream(), params, dataSource);
-        Resource body = new ByteArrayResource(bytes);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(file.getInputStream(), params, dataSource);
+        JRPdfExporter pdf = new JRPdfExporter();
+        pdf.setExporterInput(new SimpleExporterInput(jasperPrint));
+        pdf.setExporterOutput(new SimpleOutputStreamExporterOutput(out));
+        SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
+        pdf.setConfiguration(configuration);
+        pdf.exportReport();
+        Resource body = new ByteArrayResource(out.toByteArray());
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename().replace("jasper", "pdf")+"\"").body(body);
     }
 }
