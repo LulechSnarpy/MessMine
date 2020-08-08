d105ed60de66e2fcde4982cf9d8b9060c2f56b57
lulech <1272045703@qq.com>
2020-08-08 17:13:12 +0800

    a project start with gradle need add spring boot and vue

---
+++ painting-timer-web/src/main/java/org/iskyc/lulech/main/Main.java
@@ -0,0 +1,10 @@
+package org.iskyc.lulech.main;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
