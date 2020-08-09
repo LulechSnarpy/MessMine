12601393f233ca94bc5e8b4abdc586c906c793ec
lulech <1272045703@qq.com>
2020-08-09 16:04:48 +0800

    run with hello world but config stay not work

--- painting-timer-web/src/main/java/org/iskyc/lulech/Application.java
+++ painting-timer-web/src/main/java/org/iskyc/lulech/Application.java
@@ -1,19 +1,21 @@
-package org.iskyc.lulech.main;
+package org.iskyc.lulech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Main {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
