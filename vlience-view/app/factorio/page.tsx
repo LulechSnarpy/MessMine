f55187a11322a6b8217e79119d6e00975b92c5d6
LulechSnarpy <lulechsnarpy@gmail.com>
2024-08-30 11:09:08 +0800

    Add canvas konva react-konva Run a demo on it

--- vlience-view/app/factorio/page.tsx
+++ vlience-view/app/factorio/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+import dynamic from "next/dynamic";
+
+const Canvas = dynamic(() => import("@/app/components/canvas"),{
+  ssr: false,
+});
+
 export default function Page() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="mb-3 text-2xl font-semibold">Main Page</h1>
         <div>
-          <canvas></canvas>
+          <Canvas />
         </div>
       </main>
     );
